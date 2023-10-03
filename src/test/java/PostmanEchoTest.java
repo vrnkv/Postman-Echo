79487d7d6292f6ad904d52a775ef99bc11842b8e
vrnkv <vrnkv2014@yandex.ru>
2023-10-03 15:35:41 +0400

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData () {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"));
+    }
+}
